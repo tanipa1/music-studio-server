1c651177886d748d6853166aa3813ecfa3d43249
tanipa1 <tania35.291@gmail.com>
2023-06-08 20:11:32 +0600

    Music studio server running

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewear
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Music Studio is running')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Music Studio is listening on port ${port}`)
 })
